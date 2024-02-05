ba01f51ba12b1c93964b3405bc67930e20be9ea9
Владимир <vladimir_udalev@mail.ru>
2024-02-05 09:15:29 +0500

    ticket

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
-public class Main {
+import java.util.Scanner;
+
+    class PalindromeChecker {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите строку: ");
+        String input = scanner.nextLine();
+
+        if (isPalindrome(input)) {
+            System.out.println("Это палиндром.");
+        } else {
+            System.out.println("Это не палиндром.");
+        }
+    }
+
+    private static boolean isPalindrome(String str) {
+        str = str.replaceAll("[^a-zA-Z]", "").toLowerCase();
+        String reversed = new StringBuilder(str).reverse().toString();
+        return str.equals(reversed);
     }
 }
